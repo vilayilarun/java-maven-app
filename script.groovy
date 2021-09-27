fa7e258d96a40a7e12ec6906e0bdfb49b98ea63c
vilayilarun <vilayilarun@gmail.com>
2021-09-27 13:43:37 +0530

    Typo corrected in script.groovy

--- script.groovy
+++ script.groovy
@@ -4,7 +4,7 @@ def buildjar() {
 def imagebuild() {
     sh 'docker build -t 192.168.179.131:8083/java-manen-app:1.1'
 }
-drf imagepush() {
+def imagepush() {
     withCredentials([usernamePassword(credentialsId: 'Nexus-repo', usernameVariable: 'USER', passwordVariable: 'PWD')]){
     sh "echo $PWD | docker login -u $USER --pasword-stdin 192.168.179.131:8083"
     sh 'docker push 192.168.179.131:8083/java-manen-app:1.1'
