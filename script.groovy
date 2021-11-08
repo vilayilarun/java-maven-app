7600e7cd8de8394f7b3b8829b3a0f9553c97a144
vilayilarun <vilayilarun@gmail.com>
2021-11-08 23:05:04 +0400

    updated hostkey checking

--- script.groovy
+++ script.groovy
@@ -25,8 +25,8 @@ def deploy() {
     def ec2Instance = "ec2-user@3.108.220.227"
 
     sshagent(['ec2-aws']) {
-        sh "scp server-cmds.sh ${ec2Instance}:/home/ec2-user"
-        sh "scp docker-compose.yaml ${ec2Instance}:/home/ec2-user"
+        sh "scp -o StrictHostKeyChecking=no server-cmds.sh ${ec2Instance}:/home/ec2-user"
+        sh "scp -o StrictHostKeyChecking=no docker-compose.yaml ${ec2Instance}:/home/ec2-user"
         sh "ssh -o StrictHostKeyChecking=no ${ec2Instance} ${shellCmd}"
 
     }
