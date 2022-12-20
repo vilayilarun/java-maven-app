6e4553a60f06977c407fbc6e7b2d9d8f16993667
vilayilarun <32998015+vilayilarun@users.noreply.github.com>
2022-12-20 18:53:41 +0400

    Update server-cmds.sh

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export IMG=35.200.245.75:8083/java-maven:$1
+export IMG=vilayilarun/max:java-maven-$1
 export DOCKER_PWD=$2
 export DOCKER_USR=$3
-echo $DOCKER_PWD | docker login -u $DOCKER_USR 35.200.245.75:8083 --password-stdin
+echo $DOCKER_PWD | docker login -u $DOCKER_USR --password-stdin
 docker-compose -f docker-compose.yaml up -d
