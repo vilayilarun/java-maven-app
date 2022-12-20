f5fd89cc434e58b8b62444bc2aca6038daa37c41
vilayilarun <32998015+vilayilarun@users.noreply.github.com>
2022-12-20 16:28:45 +0400

    Update script.groovy
    Script updated

--- script.groovy
+++ script.groovy
@@ -27,7 +27,6 @@ def provisioning() {
           returnStdout: true
         ).trim()
     }
-#}
 def deploy() {
     withCredentials([usernamePassword(credentialsId: 'nexus-repo', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PWD')]){
     echo "waiting for the instance is come up"
